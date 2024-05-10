ace08d160a1e90afe2606e3db96f133421c46ba7
Kayte99 <katherinediri9@gmail.com>
2024-05-10 09:46:25 +0100

    i deleted a code

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-// let input = document.getElementById ( 'inputBox');
-// let button = document.querySelectorAll('button');
-// // // let clear = document.querySelectorAll(".clear");
-// let equals = document.querySelectorAll(".equals-button");
-
-// let string = "";
-// let arr = Array.from(button)
-// arr.forEach(button =>{
-//      button.addEventListener( 'click', (e) =>{
-//       if(e.target.innerHTML == '='){
-//         string = eval( string);
-//         input.value = string;
-//       }
-      
-//     else{
-//         string += e.target.innerHTML;
-//         input.value = string;
-//       }
-     
-//      }) 
-// })
-
 let displayed = document.getElementById('inputBox')
 
 function display(num){
